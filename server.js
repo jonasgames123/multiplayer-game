550225583986263a0e3267851b48699db2250dbd
jonasgames123 <jonasprzygoda6@gmail.com>
2026-01-17 22:42:01 +0100

    server.js

--- server.js
+++ server.js
@@ -12,12 +12,18 @@ let players = {};
 
 wss.on("connection", (ws) => {
   const id = Math.random().toString(36).substr(2, 9);
-  players[id] = { x: 100, y: 100 };
+
+  players[id] = { x: 200, y: 200 };
+
+  // 👉 schick dem Spieler seine ID
+  ws.send(JSON.stringify({ type: "id", id }));
 
   ws.on("message", (msg) => {
     try {
       const data = JSON.parse(msg);
-      players[id] = data;
+      if (data.type === "move") {
+        players[id] = data.player;
+      }
     } catch (e) {}
   });
 
@@ -27,7 +33,7 @@ wss.on("connection", (ws) => {
 });
 
 setInterval(() => {
-  const data = JSON.stringify(players);
+  const data = JSON.stringify({ type: "players", players });
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(data);
@@ -37,4 +43,3 @@ setInterval(() => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Server läuft"));
-
