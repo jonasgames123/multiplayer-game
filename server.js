88e06736a374b005450e9238b11e978cb54c92ef
jonasgames123 <jonasprzygoda6@gmail.com>
2026-01-17 22:29:48 +0100

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 let players = {};
 
@@ -37,3 +37,4 @@ setInterval(() => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Server läuft"));
+
